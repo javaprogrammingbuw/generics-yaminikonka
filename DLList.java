51300de5aae7083427132d11f64ba493eda22238
Konka Yamini <yamini.konka@gmail.com>
2019-04-03 21:35:04 +0530

    please check the generics exercise, thank you

---
+++ DLList.java
@@ -0,0 +1,216 @@
+
+
+public class DLList<D>{
+     private Node<D> head;
+     // private Node<T> next;
+  // public DCList(Node<T> head, Node<T> next){
+  // 	this.head=head;
+  // 	this.next=next;
+  // } 
+     public DLList(){
+     // 	head.previousNode = null;
+        // head.position = 0;
+        head = null;
+     }
+    public boolean isEmpty(){
+    	if(head==null){
+    		return true;
+    	}else{
+    		return false;
+    	}
+    }
+    public void display(){
+        Node<D> current = head;
+        while(current!=null){
+            System.out.println(current.getValue());
+            current = current.getNextNode();
+        }
+        // /System.out.println(current.getValue());
+    }
+    public void add(int position,D value){
+        Node<D> current = new Node<>(value);
+    
+
+            if(position==0) {
+
+                current.setNextNode(head);
+
+                head.setPreviousNode(current);
+
+                head = current;
+
+            } 
+        
+        Node<D> currentnext = get(position);
+           currentnext.getPreviousNode().setNextNode(current);
+            current.setPreviousNode(currentnext.getPreviousNode());
+              current.setNextNode(currentnext);
+                currentnext.setPreviousNode(current);
+        current.position = position;
+           Node<D> next= current.getNextNode();
+        while(next!=null){
+            // current = current.getNextNode();
+            // current
+            
+            next.position++;
+            next=next.getNextNode();
+            // current = current.getNextNode();
+        }
+        
+
+
+    }
+    public void add(D value){
+         // head.position=0;
+        int index = size();
+    	Node<D> next = new Node<D>(value);
+    		next.nextNode=null;
+    	if(head != null){
+            Node<D> last = get(size()-1);
+             last.setNextNode(next);
+             next.setPreviousNode(last);
+             // last.position = last.getPreviousNode().position+1;
+             // next.position = last.position+1;
+         }else{
+            head = next;
+         }
+         next.position = index;
+
+    }
+    public void remove(D value){
+         // head.value=next.value;
+         // head.previous=
+        // Node<D> current = head;
+        // while(current!= null){
+        //     if(current.value==value){
+        //         current.getPreviousNode().setNextNode(current.getNextNode());
+        //         current.getNextNode().setPreviousNode(current.getPreviousNode());
+        //         // current = current.getNextNode();
+
+        //     }
+
+        //     current = current.getNextNode();    
+        // }    
+            // while(current!=null){
+            //     current.position=index;
+            //     index=index++;
+            // }
+        
+
+
+        //  second way
+
+          int index = find(value);
+        Node<D> current = get(index);
+            current.getPreviousNode().setNextNode(current.getNextNode());
+            current.getNextNode().setPreviousNode(current.getPreviousNode());
+            current=current.getNextNode();
+            while(current!=null){
+                current.position=index;
+                index=index++;
+                current=current.getNextNode();
+            }
+          
+    } 
+    public void removeFirst(){
+        // Node<D> current = head;
+        // current = current.getNextNode();
+         // head.setNextNode(current);
+         // current.setPreviousNode(head);
+        // Node<D> first = head.getNextNode();
+        // first = head.getNextNode();
+        head = head.getNextNode();
+        head.setPreviousNode(null);
+        // int count = 0;
+        // first.position = 0;
+        // while(first!=null){
+        //     first = first.getNextNode();
+        //     first.position--;
+        //     // count = first position;
+        //     // count++;
+        // }
+
+    }
+    public void removeLast(){
+        Node<D> current = head;
+        while(current.getNextNode()!=null){
+            current = current.getNextNode();
+        } 
+        // current.getPreviousNode().setNextNode(null);
+        current.setNextNode(null);
+
+        //  we can do in the following way
+
+        // Node<D> last = get(size() - 1);
+        // last.getPreviousNode().setNextNode(null);
+
+    }
+    public void clear(){
+        head=null;
+
+    }
+    public Node<D> getFirst(){
+        if(isEmpty() == true){
+            return null;
+        }else{
+            return head;
+        }
+
+    }
+    public Node<D> getLast(){
+      Node<D> current=head;
+      while(current.nextNode!=null){
+        current = current.getNextNode();
+      }
+      return current;
+
+      // or we can do like this also
+    //   Node<D> last = get(size() - 1);
+    //   return last;
+    }
+    public Node<D> get(int position){
+        Node<D> current=head;
+        while(current != null){
+            if(current.position==position){
+                return current;
+            }
+            current = current.getNextNode();
+        }
+        return null;
+        
+
+    }
+    public int find(D value){
+        Node<D> current=head;
+        while(current!=null){
+            if(current.value == value){
+                return current.position;
+            }
+            current = current.getNextNode();
+        }
+        return -1;
+
+    }
+    public boolean contains(D value){
+        Node<D> current = head;
+        while(current!=null){
+            if(current.value==value){
+                return true;
+            }
+            current = current.getNextNode();
+        }
+        return false;
+        
+
+    }
+    public int size(){
+        Node<D> current = head;
+        int count = 0;
+       while(current!=null){
+            count++;
+            current = current.getNextNode();
+       }
+       return count;
+
+    }
+}
--- Driver.java
+++ Driver.java
@@ -2,17 +2,47 @@
 
 public class Driver{
 	public static void main(String[] args){
-		Node<Integer> node=new Node<>(4);
-	    DCList <Integer> list=new DCList<>();
-		  list.add(new Node<>(6));
-		  list.add(new Node<>(3));
-		  list.add(new Node<>(21));
-		  list.display();
-		  list.removeFirst();
-		  list.removeLast();
-		  list.find(6);
-		  list.contains(21);
-		  list.clear();
+		// Node<Integer> node=new Node<>(4);
+	    DLList <Integer> list=new DLList<>();
+	    list.add(5);
+	    list.add(7);
+	    list.add(10);
+	    list.add(11);
+	    list.add(87);
+	    list.add(21);
+	    list.add(12);
+	    list.add(9);
+	    list.display();
+	    System.out.println("the size of the list is: "+list.size());
+	    System.out.println("the position of the value 21 in the list is: "+list.find(21));
+	    System.out.println("the list after adding value at particular position: ");
+	    list.add(2,32);
+	    list.display();
+	    System.out.println("the list after removing the value: ");
+	    list.remove(10);
+	    list.display();
+	    boolean yam=list.contains(21);    // try with the any other elements, which are not present in the list;
+        if(yam==true){
+        	System.out.println("it is there in the list: \n");
+        }else{
+        	System.out.println("it is not there in the list: \n");
+        }
+        System.out.println("after removing the first node: ");
+        list.removeFirst();
+        list.display();
+        System.out.println("removing the last node: ");
+        list.removeLast();
+        list.display();
+        System.out.println("getting the firstnode value: "+list.getFirst().value);
+        System.out.println("the size : "+ list.size()+"\n");
+        System.out.println("getting the lastnode value: "+list.getLast().value+"\n");
+        System.out.println("after clearing the list: ");
+        list.clear();
+        System.out.println("new list after clearing the past list: ");
+        list.add(21);
+        list.add(76);
+        list.add(13);
+        list.display();
 		  
 		  
 	}
